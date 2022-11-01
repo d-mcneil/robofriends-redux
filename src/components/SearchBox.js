4c576605ee23fdcfe05580b177bfec58f7490dbf
Dean McNeil <deanolmcneil@gmail.com>
2022-11-01 16:41:35 -0600

    Optimizations: label for search box and swap for font-display

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const SearchBox = ({ onSearchChange }) => {
-    return(
-        <div className='pa2'>
-            <input 
-            className='pa3 ba b--green bg-lightest-blue'
-            type='search' 
-            placeholder='Search RoboFriends'
-            onChange={onSearchChange}
-            ></input>
-        </div>
-    )
-}
+  return (
+    <div className="pa2">
+      <input
+        aria-label="Search RoboFriends"
+        className="pa3 ba b--green bg-lightest-blue"
+        type="search"
+        placeholder="Search RoboFriends"
+        onChange={onSearchChange}
+      ></input>
+    </div>
+  );
+};
 
 export default SearchBox;
